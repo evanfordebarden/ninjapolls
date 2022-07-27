b775b08707100e5e34f12fb374d652c0b2e8f0e2
efb <evanbarden@gmail.com>
2022-07-27 17:47:37 -0400

    completes lesson 30 - Lifecycle Hooks

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,15 +1,36 @@
 <script>
+  // *Below line required for subscribe/unsub long way* //
+  // import { onMount, onDestroy } from "svelte";
+
   import PollStore from "../stores/PollStore";
   import PollDetails from "./PollDetails.svelte";
-  export let polls = [];
 
-  PollStore.subscribe((data) => {
-    polls = data;
-  });
+  // *No longer need below if referencing store directly* //
+  // export let polls = [];
+
+  // *Subscribes to PollStore AND returns an unsubscribe function* //
+  // const unsub = PollStore.subscribe((data) => {
+  // updates local polls variable
+  //   polls = data;
+  // });
+
+  // *LIFECYCLE HOOKS* //
+
+  // // onMount(() => {
+  // //   // maybe get data from a db
+  // //   console.log("component mounted");
+  // // });
+
+  // onDestroy(() => {
+  //   // unsub from store
+  //   console.log("component destroyed");
+  //   unsub();
+  // });
 </script>
 
 <div class="poll-list">
-  {#each polls as poll (poll.id)}
+  <!-- $PollStore below subscribes and unsubscribes us from the store -->
+  {#each $PollStore as poll (poll.id)}
     <div><PollDetails {poll} on:vote /></div>
   {/each}
 </div>
