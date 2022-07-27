06a7235154ae5f150baa217708699d53750d3e9c
efb <evanbarden@gmail.com>
2022-07-27 18:10:02 -0400

    completes lesson 31 - Updating Store Data

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -1,4 +1,5 @@
 <script>
+  import PollStore from "../stores/PollStore";
   import { createEventDispatcher } from "svelte";
   import Button from "../shared/Button.svelte";
 
@@ -40,7 +41,11 @@
     // add new poll
     if (valid) {
       let poll = { ...fields, votesA: 0, votesB: 0, id: Math.random() };
-      dispatch("add", poll);
+      // save poll to store
+      PollStore.update((currentPolls) => {
+        return [poll, ...currentPolls];
+      });
+      dispatch("add");
     }
   };
 </script>
--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,37 +1,12 @@
 <script>
-  // *Below line required for subscribe/unsub long way* //
-  // import { onMount, onDestroy } from "svelte";
-
   import PollStore from "../stores/PollStore";
   import PollDetails from "./PollDetails.svelte";
-
-  // *No longer need below if referencing store directly* //
-  // export let polls = [];
-
-  // *Subscribes to PollStore AND returns an unsubscribe function* //
-  // const unsub = PollStore.subscribe((data) => {
-  // updates local polls variable
-  //   polls = data;
-  // });
-
-  // *LIFECYCLE HOOKS* //
-
-  // // onMount(() => {
-  // //   // maybe get data from a db
-  // //   console.log("component mounted");
-  // // });
-
-  // onDestroy(() => {
-  //   // unsub from store
-  //   console.log("component destroyed");
-  //   unsub();
-  // });
 </script>
 
 <div class="poll-list">
   <!-- $PollStore below subscribes and unsubscribes us from the store -->
   {#each $PollStore as poll (poll.id)}
-    <div><PollDetails {poll} on:vote /></div>
+    <div><PollDetails {poll} /></div>
   {/each}
 </div>
 
