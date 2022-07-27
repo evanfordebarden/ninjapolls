fbceb267650e0df4d645fabd86fabe39e0ad6d94
efb <evanbarden@gmail.com>
2022-07-27 17:04:10 -0400

    completes lessons 26 & 27 - Card Component & Casting Votes

--- src/components/PollDetails.svelte
+++ src/components/PollDetails.svelte
@@ -1,22 +1,32 @@
 <script>
+  import { createEventDispatcher } from "svelte";
+  import Card from "../shared/Card.svelte";
   export let poll;
+  const dispatch = createEventDispatcher();
 
   // reactive values
   $: totalVotes = poll.votesA + poll.votesB;
+
+  //handling votes
+  const handleVote = (option, id) => {
+    dispatch("vote", { option, id });
+  };
 </script>
 
-<div class="poll">
-  <h3>{poll.question}</h3>
-  <p>Total votes: {totalVotes}</p>
-  <div class="answer">
-    <div class="percent percent-a" />
-    <span>{poll.answerA} ({poll.votesA})</span>
+<Card>
+  <div class="poll">
+    <h3>{poll.question}</h3>
+    <p>Total votes: {totalVotes}</p>
+    <div class="answer" on:click={() => handleVote("a", poll.id)}>
+      <div class="percent percent-a" />
+      <span>{poll.answerA} ({poll.votesA})</span>
+    </div>
+    <div class="answer" on:click={() => handleVote("b", poll.id)}>
+      <div class="percent percent-b" />
+      <span>{poll.answerB} ({poll.votesB})</span>
+    </div>
   </div>
-  <div class="answer">
-    <div class="percent percent-b" />
-    <span>{poll.answerB} ({poll.votesB})</span>
-  </div>
-</div>
+</Card>
 
 <style>
   h3 {
