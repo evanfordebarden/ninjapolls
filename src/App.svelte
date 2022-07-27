ddb1e7455dcc32812180c91b4b55a16411041805
efb <evanbarden@gmail.com>
2022-07-27 14:34:04 -0400

    completes lesson 19 - Header & Footer Components

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,22 @@
 <script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
 </script>
 
+<Header />
 <main>
-  <h1>Hello, Ninjas!</h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas vel
+    quaerat ad distinctio dolor, molestiae recusandae animi voluptatibus,
+    repellendus aliquam quis mollitia. Unde tenetur ipsum, similique odio
+    cupiditate eius nam.
+  </p>
 </main>
+<Footer />
 
 <style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
 </style>
